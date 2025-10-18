777a8ec2aaf1fb4ee199f0921ed9b86739709380
rotlug <rotlug77@gmail.com>
2025-10-19 01:58:40 +0300

    Initial Commit

---
+++ lua/myLuaConf/format.lua
@@ -0,0 +1,43 @@
+require("lze").load({
+	{
+		"conform.nvim",
+		for_cat = "format",
+		cmd = { "ConformInfo" },
+		event = { "BufReadPre", "BufNewFile" },
+		keys = {
+			{ "<leader>FF", desc = "[F]ormat [F]ile" },
+		},
+		-- colorscheme = "",
+		after = function(plugin)
+			local conform = require("conform")
+
+			conform.setup({
+				default_format_opts = {
+					lsp_format = "never",
+				},
+				format_on_save = {
+					-- These options will be passed to conform.format()
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				},
+				formatters_by_ft = {
+					lua = { "stylua" },
+					nix = { "alejandra" },
+					python = { "ruff_format", "ruff_fix" },
+					javascript = { "prettierd" },
+					typescript = { "prettierd" },
+					javascriptreact = { "prettierd" },
+					typescriptreact = { "prettierd" },
+				},
+			})
+
+			vim.keymap.set({ "n", "v" }, "<leader>FF", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			end, { desc = "[F]ormat [F]ile" })
+		end,
+	},
+})
