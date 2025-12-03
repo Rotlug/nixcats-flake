f99b9200982f0303832a313d1dc0197b4160a3b8
rotlug <rotlug77@gmail.com>
2025-12-03 15:59:06 +0200

    Bring back hidden buffers

--- lua/myLuaConf/opts_and_keys.lua
+++ lua/myLuaConf/opts_and_keys.lua
@@ -191,6 +191,3 @@ vim.keymap.set("x", "<M-f>", function()
 	-- Feed keys so command-line shows with cursor between the slashes
 	vim.api.nvim_feedkeys(final, "n", false)
 end, { noremap = true, desc = "Global substitute of selected text (Alt-f)" })
-
--- No hidden buffers
-vim.opt.hidden = false
